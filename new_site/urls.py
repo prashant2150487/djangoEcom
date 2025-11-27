ee4fe9e5c801442731fa24b41afa1471503a63ef
Prashant Sachan <your-email@example.com>
2025-11-27 23:57:03 +0530

    feat: Implement initial store application with product listing view and base templates.

--- new_site/urls.py
+++ new_site/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 import os
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,7 +8,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls', namespace='store'))
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Category, Product
+
+def categories(request):
+    categories= Category.objects.all()
+    return categories
+
+def all_products(request):
+    products= Product.objects.all()
+    return render(request, "store/home.html", {'products':Product})
