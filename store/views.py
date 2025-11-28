672ea605d492ba2987336cb4f3bcb6cf6f2a1d09
Prashant Sachan <your-email@example.com>
2025-11-28 09:07:20 +0530

    feat: Add `.gitignore` file and refactor views to correctly pass context data to templates.

--- store/views.py
+++ store/views.py
@@ -5,9 +5,8 @@ from django.shortcuts import render
 from .models import Category, Product
 
 def categories(request):
-    categories= Category.objects.all()
-    return categories
+    return { "categories" : Category.objects.all() }
 
 def all_products(request):
     products= Product.objects.all()
-    return render(request, "store/home.html", {'products':Product})
+    return render(request, "store/home.html", {'products': products})
